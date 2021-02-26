ea4b2c86aef73171ae743b8ce74ccd8bf0bdeda0
Francine5424 <77037619+Francine5424@users.noreply.github.com>
2021-02-26 21:52:46 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<html><head> <title> FIGHT FOR FUTURE </title>
+</head><body bgcolor="#DEB887"> 
+
+
+<font face="times new roman" ,="" color="navy" size="15">
+<marquee direction="right"> NEVER LOSING HOPE </marquee></font>
+<font face="times new roman" ,="" color="#7B68EE" size="15">
+<marquee> AGAINST COVID-19 </marquee></font>
+<center>
+ <audio controls="">
+  
+  <source src="https://youtu.be/d5w0lHMHFSo" type="audio/mpeg"> 
+ </audio>
+
+<font face="courier" ,="" color="#A52A2A" size="7">
+<h1> TODAY </h1>
+</font>
+<font face="optima" ,="" color="2F4F4F" size="4">
+<b>Today we faced so many problem.</b> <br>
+<b>And one the biggest problem that most of the people facing is the Pandemic.</b><br> 
+<b> But never lose hope because every problem has a solution.</b><br><br><br>
+<img src="49749284_292883044702070_6717237690263142400_n.jpg" ,="" length="200" width="300"><br><br><br>
+<b> This event leaves a big impact to all, until now. This virus is the reason why we are in this situation right now. </b><br>
+<b> I think this Pandemic will leave us a memorable nightmare.</b><br>
+<b>But the pandemic teaches us new way of learning without going to school. And this pandemic teaches us to clean our surrounding <br><br> 
+</b></font><b>
+
+<font face="times new roman" ,="" color="DAA520" size="6">
+<b> “Hope is being able to see that there is light despite all of the darkness.”<br> 
+<i> -Desmond Tutu </i> <br><br><br><br> 
+</b></font></b></center></body></html>
