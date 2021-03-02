3e8841bb9c39dffdf596aa2cc0370c7e6dd4319c
Francine5424 <77037619+Francine5424@users.noreply.github.com>
2021-03-02 20:28:49 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<html><head>
+ <title> FIGHT FOR FUTURE </title>
+</head><body background="155430592_767643630550152_6320746709949852456_n.jpg">
+
+<font face="Algerian" ,="" color="light green" size="17">
+<marquee direction="left"> NEVER LOSING HOPE </marquee></font>
+<font face="Algerian" ,="" color="green" size="20">
+<marquee direction="right"> AGAINST COVID-19 </marquee></font>
+<center>
+ 
+<font face="Berlin Sans FB " ,="" color="#A52A2A" size="8">
+<h1> TODAY </h1>
+</font>
+<font face="Bahnschrift SemiBold" ,="" color="2F4F4F" size="4">
+<b>Today we faced so many problem.</b> <br>
+<b>And one the biggest problem that most of the people facing is the Pandemic.</b><br> 
+<b> But never lose hope because every problem has a solution.</b><br><br><br><p>
+
+</p></font><p align="center"><font face="Bahnschrift SemiBold" ,="" color="2F4F4F" size="4">
+<b> This event leaves a big impact to all, until now. This virus is the reason why we are in this situation right now. </b><br>
+<b> I think this Pandemic will leave us a memorable nightmare.</b><br>
+<b>But the pandemic teaches us new way of learning without going to school. And this pandemic teaches us to clean our surrounding <br><br> 
+</b></font><b>
+ 
+<audio controls="">
+ </audio></b></p><p align="left"><b>
+  <source src="Rachel Platten - Fight Song (Official Video)(MP3_70K).mp3" type="audio/mpeg"> 
+ 
+</b></p><p align="center"><b>
+<img src="49749284_292883044702070_6717237690263142400_n.jpg" ,="" length="500" width="500"><br><br><br>
+<font face=" Bahnschrift SemiBold=" "="" color="orange" size="6">
+<b> <i>“Hope is being able to see that there is light despite all of the darkness.”</i><br> 
+<i> -Desmond Tutu </i> <br><br><br><br> 
+</b></font></b>
+  <font face="Berlin Sans FB " ,="" color="#A52A2A" size="8"> 
+    </font></p><h1><font face="Berlin Sans FB " ,="" color="#A52A2A" size="8"> TOMORROW </font></h1> 
+  <embed src="video-1614348805.mp4">
+    </center>
+
+</body></html>
